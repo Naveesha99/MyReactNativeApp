93b17cb0fada4bf3ad8481e91ef2673acae40b67
Naveesha Lakshan <lakshannaveesha10@gmail.com>
2025-02-17 16:56:05 +0530

    New commit

--- App.tsx
+++ App.tsx
@@ -61,7 +61,7 @@ function App(): React.JSX.Element {
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
+  
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
